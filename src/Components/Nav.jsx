40d9f9dd48a133fc618612953c60cf92122c0d32
AtharvSirsalkar <atharvsirsalkar96@gmail.com>
2024-07-03 18:01:19 +0530

    /create page error resolved

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -17,12 +17,13 @@ function Nav() {
 
   return (
     <nav className="h-full w-[15%] bg-zinc-50 flex flex-col items-center pt-5 ">
-      <a
+      <Link
+        to={`/create`}
         className="py-3 px-5 border rounded border-blue-400 hover:scale-[1.1] ease-in-out duration-700 hover:shadow-lg text-blue-400 hover:text-blue-700 hover:border-blue-800 hover:font-semibold"
-        href="/create"
+        
       >
         Add New Product
-      </a>
+      </Link>
       <hr className="my-3 w-[80%]" />
       <h1 className="w-[80%] text-2xl mb-3">Categories</h1>
       <div className="w-[80%]">
